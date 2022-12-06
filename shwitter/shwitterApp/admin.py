bc7d1d3892e8ed576792a0eddd2c96c25b6aa952
Sharon Nderi <nderisharonw@gmail.com>
2022-12-06 15:20:45 +0300

    Create admin portal and and create 3 users

--- shwitter/shwitterApp/admin.py
+++ shwitter/shwitterApp/admin.py
@@ -1,3 +1,11 @@
+""" Module to register models """
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username"]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
