420a39b0b04befcc4b57dd9fc7e508437c52b138
william-4 <mwangiiwilliam@gmail.com>
2022-12-10 09:25:58 +0300

    minor fixes

--- shwitter/shwitterApp/views.py
+++ shwitter/shwitterApp/views.py
@@ -3,7 +3,10 @@ from .models import Profile
 
 # Create your views here.
 def dashboard(request):
-    return render(request, "base.html")
+    return render(
+        request, 
+        "base.html"
+    )
 
 def profile_list(request):
     profiles = Profile.objects.exclude(user=request.user)
@@ -11,7 +14,7 @@ def profile_list(request):
         request, 
         "shwitter/profile_list.html", 
         {"profiles": profiles}
-        )
+    )
 
 def profile(request, pk):
     profile = Profile.objects.get(pk=pk)
@@ -19,4 +22,4 @@ def profile(request, pk):
         request, 
         "shwitter/profile.html", 
         {"profile": profile}
-        )
+    )
